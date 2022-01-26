5d8ab2a1f95079ab01fe158b467f91354ebbeca7
Oddegamra <oddegamra@users.sourceforge.net>
2022-01-26 13:22:52 +0100

    doc: Set version to 2.0.0 in preparation for release

--- mcomix/constants.py
+++ mcomix/constants.py
@@ -8,7 +8,7 @@ import operator
 from mcomix import tools
 
 APPNAME = 'MComix'
-VERSION = '2.0.0.dev0'
+VERSION = '2.0.0'
 
 HOME_DIR = tools.get_home_directory()
 CONFIG_DIR = tools.get_config_directory()
